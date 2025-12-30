fda97f6a19440cc6fb215da3a9d469ce170580c5
nguyenhoangdanh <hoangdanh54317@gmail.com>
2025-12-30 21:54:20 +0700

    tmp

--- src/google-sheets/google-sheets.module.ts
+++ src/google-sheets/google-sheets.module.ts
@@ -2,9 +2,7 @@ import { Module, forwardRef } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GoogleSheetsService } from './google-sheets.service';
 import { HTMSheetsService } from './htm/htm-sheets.service';
-import { HTMCenterTVService } from './htm/htm-center-tv.service';
 import { HTMSheetsListenerService } from './htm/htm-sheets-listener.service';
-import { CDSheetsService } from './cd/cd-sheets.service';
 import { CDProductSheetsService } from './cd/cd-product-sheets.service';
 import { CDProductListenerService } from './cd/cd-product-listener.service';
 import { WebsocketModule } from '../websocket/websocket.module';
@@ -14,18 +12,14 @@ import { WebsocketModule } from '../websocket/websocket.module';
   providers: [
     GoogleSheetsService,
     HTMSheetsService,
-    HTMCenterTVService,
     HTMSheetsListenerService,
-    CDSheetsService,
     CDProductSheetsService,
     CDProductListenerService,
   ],
   exports: [
     GoogleSheetsService,
     HTMSheetsService,
-    HTMCenterTVService,
     HTMSheetsListenerService,
-    CDSheetsService,
     CDProductSheetsService,
     CDProductListenerService,
   ],
